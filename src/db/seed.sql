1de73167c23c366ca85e90dcc5c31127d43b2ae1
Caleb Colangelo <calebc041406@gmail.com>
2025-02-03 12:20:57 -0500

    added starter develop code and completed the DB

---
+++ src/db/seed.sql
@@ -0,0 +1,20 @@
+INSERT INTO department (department_name)
+VALUES 
+    ('Customer Service'),
+    ('Sales');
+
+SELECT * FROM department;
+
+INSERT INTO role (title, salary)
+VALUES 
+    ('Customer Service Specialist', 75.000),
+    ('Sales Lead', 50.000);
+
+SELECT * FROM role;
+    
+INSERT INTO employee (first_name, last_name)
+VALUES
+    ('John', 'Doe'),
+    ('Caleb', 'Colangelo');
+
+SELECT * FROM employee;
